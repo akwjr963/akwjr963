db22cae7113b67ef4697c0a20bfbe3746f912290
Jiwoo_Chw <cn686700@gmail.com>
2022-07-12 12:03:28 +0900

    gd

--- server/routes/QA_client.js
+++ server/routes/QA_client.js
@@ -2,14 +2,14 @@ const express = require("express")
 const router = express.Router()
 const DB = require("")
 
-router.get("/client/question?category=??&word=??",async (req, res) => {
-    const { category, title } = req.query
-    const list = DB.find({ 
-        category,
-        title: word
-    })
-    
-    res.json({
-        list
-    })
-})
+// router.get("/client/question?category=??&word=??",async (req, res) => {
+//     const { category, title } = req.query
+//     const list = DB.find({ 
+//         category,
+//         title: word
+//     })
+
+//     res.json({
+//         list
+//     })
+// })
